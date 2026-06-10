a5fb123bdb2d77f613ef47f34c19d8e051349d31
angpavv-star <angpavv@gmail.com>
2026-06-10 17:55:55 +0500

    автопубликация релиза на GitHub

---
+++ scripts/publish-release.js
@@ -0,0 +1,12 @@
+const { execSync } = require('child_process')
+const { version } = require('../package.json')
+
+const tag = `v${version}`
+const exe = `release/Учет выплат Setup ${version}.exe`
+const blockmap = `release/Учет выплат Setup ${version}.exe.blockmap`
+const yml = `release/latest.yml`
+
+console.log(`Публикация релиза ${tag} на GitHub...`)
+const gh = process.platform === 'win32' ? '"C:\\Program Files\\GitHub CLI\\gh.exe"' : 'gh'
+execSync(`${gh} release create ${tag} "${exe}" "${yml}" "${blockmap}" --title "${tag}" --notes ""`, { stdio: 'inherit' })
+console.log(`Готово: ${tag}`)
