dc8f4eda25e3541a9617b2beab0e35bb7f0c06c4
angpavv-star <angpavv@gmail.com>
2026-06-10 18:12:14 +0500

    fix: push тега перед публикацией релиза

--- scripts/publish-release.js
+++ scripts/publish-release.js
@@ -6,6 +6,9 @@ const exe = `release/Учет выплат Setup ${version}.exe`
 const blockmap = `release/Учет выплат Setup ${version}.exe.blockmap`
 const yml = `release/latest.yml`
 
+console.log(`Отправка тега ${tag} на GitHub...`)
+execSync(`git push origin dev --tags`, { stdio: 'inherit' })
+
 console.log(`Публикация релиза ${tag} на GitHub...`)
 const gh = process.platform === 'win32' ? '"C:\\Program Files\\GitHub CLI\\gh.exe"' : 'gh'
 execSync(`${gh} release create ${tag} "${exe}" "${yml}" "${blockmap}" --title "${tag}" --notes ""`, { stdio: 'inherit' })
